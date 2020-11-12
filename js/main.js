89b4c91e886bd4d9efab0ecb2a1877d323c2c297
Temirlan Ismukhanov <temirlanismukhanov@Temirlans-MacBook-Pro.local>
2020-11-12 17:54:45 -0600

    cleaning of semantic css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
 
 $( document ).ready(function() {
+
+
     $('.menu .item').tab();
     $('.ui.modal').modal();
 
@@ -39,6 +55,8 @@ $( document ).ready(function() {
         });
         
     });
+
+
 });
 
 
